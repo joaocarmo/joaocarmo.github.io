d7b8c0fa868809dedc8af3ed25b6681046472209
joaocarmo <kingjoao@gmail.com>
2016-07-13 02:05:08 +0100

    Final presenting page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  $.backstretch('../images/background.jpg', {speed: 150});  
+  $.backstretch('../images/background.jpg', {speed: 150});
+  $(window).resize(function(){
+  	$('#app').css({
+  		position: 'absolute',
+  		left: ($(window).width() - $('#app').outerWidth())/2,
+  		top: ($(window).height() - $('#app').outerHeight())/2
+  	});
+  });
 });
